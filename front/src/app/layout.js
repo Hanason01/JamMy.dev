5a63cb3fae4977c1007078d9dbd24e9d3bb58333
pucha <pucha@puchanoMac-mini.local>
2024-11-28 00:02:48 +0900

    layout.jsのimport漏れ解消

--- front/src/app/layout.js
+++ front/src/app/layout.js
@@ -1,6 +1,7 @@
 import { ThemeProviderWrapper } from '../components/ThemeProviderWrapper';
 import { AuthProvider } from '../context/useAuthContext';
 import { AuthModal } from '../components/User/AuthModal';
+import { Suspense } from'react';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
