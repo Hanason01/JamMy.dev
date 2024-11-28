4b07b3a6e22abe4926081ae6ac1aa7584e5bfa1e
pucha <pucha@puchanoMac-mini.local>
2024-11-29 01:24:58 +0900

    独自ドメイン必須の為、middleware.jsによる制御を一旦停止

--- back/config/initializers/devise_token_auth.rb
+++ back/config/initializers/devise_token_auth.rb
@@ -70,6 +70,6 @@ DeviseTokenAuth.setup do |config|
     httponly: true,
     same_site: :none,
     expires: 1.day.from_now,
-    domain: ".jammy-dev"
+    domain: ENV.fetch("COOKIE_DOMAIN", "localhost")
   }
 end
--- front/src/middleware.js
+++ front/src/middleware.js
@@ -24,6 +24,8 @@ export function middleware(req) {
 
 // 認証対象のパスを指定
 export const config = {
-  matcher: ['/mypage/:path*', '/projects/:path+',
+  matcher: ['/mypage/:path*',
   ],
 };
+
+//注意書き）本ファイルはURL打ち込みによる静的なリクエストを制御するもの。クッキーの共有が前提となっており、独自ドメイン取得が必須となっている為、独自ドメイン取得が可能になった段階で「'/projects/:path+'」こちらおよび他の認証が必要なページについて対象として加える。
