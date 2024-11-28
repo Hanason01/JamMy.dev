338f280ae42849c312541d7a4e2ebd8a037aa392
pucha <pucha@puchanoMac-mini.local>
2024-11-28 15:22:07 +0900

    auth/pageにダイナミック設定

--- front/src/app/auth/page.jsx
+++ front/src/app/auth/page.jsx
@@ -1,11 +1,13 @@
 "use client";
+export const dynamic = "force-dynamic";
+
 
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 import { AuthModal } from '../../components/User/AuthModal';
 import { Suspense } from'react';
 
-export default function StaticAuthPage() {
+export default function AuthPage() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const redirectTo = searchParams.get("redirectTo") //直前のリクエストURL
