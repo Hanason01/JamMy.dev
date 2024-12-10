9afde7d1c1f69c7caac3d7de266e516d47743d58
pucha <pucha@puchanoMac-mini.local>
2024-12-10 23:50:31 +0900

    middlewareを停止

--- front/src/middleware.js
+++ front/src/middleware.js
@@ -24,8 +24,8 @@ export function middleware(req) {
 
 // 認証対象のパスを指定
 export const config = {
-  matcher: ['/projects/:path+', '/mypage/:path*', '/notification/:path*',
+  matcher: ['/projects/:path+',
   ],
 };
 
-// '/post_project/:path*'
+// '/post_project/:path*' '/mypage/:path*', '/notification/:path*'
