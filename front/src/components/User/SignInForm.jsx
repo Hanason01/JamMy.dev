3bea21e379915d823c565e7ca0282cb4bda01f8d
pucha <pucha@puchanoMac-mini.local>
2024-12-10 22:41:32 +0900

    debug

--- front/src/components/User/SignInForm.jsx
+++ front/src/components/User/SignInForm.jsx
@@ -23,10 +23,7 @@ export function SignInForm({redirectTo}) {
   const sendDataToAPI = async (data) => {
     try {
       await signIn(data);
-      if (redirectTo){
-        router.push(redirectTo);
-      }else{
-        router.push("/projects?refresh=true");
+      router.push(redirectTo || "/projects?refresh=true");
       }
 
     } catch (error) {
