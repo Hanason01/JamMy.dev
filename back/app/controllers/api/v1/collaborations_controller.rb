33d4e80b5809492017fe36a98f1d5522c31709db
pucha <pucha@puchanoMac-mini.local>
2025-01-13 19:44:08 +0900

    s3ロジックリフトアップ、collaborationのcreateメソッド定義

--- back/app/controllers/api/v1/collaborations_controller.rb
+++ back/app/controllers/api/v1/collaborations_controller.rb
@@ -1,7 +1,38 @@
 class Api::V1::CollaborationsController < ApplicationController
   before_action :authenticate_user!
-  # def new
-  #   project = Project.find(params[:project_id])
-  #   render json: ProjectSerializer.new(project, { include: [:user] }).serializable_hash
-  # end
+
+  def create
+    ActiveRecord::Base.transaction do
+      #プロジェクト作成
+      collaboration = current_user.collaborations.build(collaboration_params.except(:audio_file))
+      collaboration.project_id = params[:project_id]
+      unless collaboration.save
+        raise ActiveRecord::Rollback, collaboration.errors.full_messages.join(", ")
+      end
+
+      unless params[:collaboration][:audio_file].present?
+        raise ActiveRecord::Rollback, "音声ファイルが必要です"
+      end
+
+        #ファイル保存
+      audio_file = collaboration.build_audio_file(
+        file_path: upload_audio_file_to_s3(params[:collaboration][:audio_file])
+      )
+      unless audio_file.save
+        raise ActiveRecord::Rollback, audio_file.errors.full_messages.join(", ")
+      end
+
+      #全て成功時のレスポンス
+      render json: { message: "Collaboration and audio file created successfully" }, status: :created
+    end
+  rescue ActiveRecord::Rollback => e
+    #ロールバック時のレスポンス
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+private
+
+  def collaboration_params
+    params.require(:collaboration).permit(:comment, :audio_file)
+  end
 end
