34aeba33c34c8409f7e028460b5ad3cc712e7c18
pucha <pucha@puchanoMac-mini.local>
2025-01-27 03:14:13 +0900

    デバック

--- front/src/app/notification/page.tsx
+++ front/src/app/notification/page.tsx
@@ -1,15 +1,18 @@
 import { Header } from "@components/Header";
 import { FeedbackAlert } from "@components/FeedbackAlert";
 import { BottomNavi } from "@components/BottomNavi";
+import { Suspense } from"react";
 
 export default function Notification(){
   return(
-    <div>
-      <Header />
-      <FeedbackAlert />
-      <h1>通知画面</h1>
-      <h3>本リリースまでお待ちください...</h3>
-      <BottomNavi />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <Header />
+        <FeedbackAlert />
+        <h1>通知画面</h1>
+        <h3>本リリースまでお待ちください...</h3>
+        <BottomNavi />
+      </div>
+    </Suspense>
   );
 }
--- front/src/app/terms/page.tsx
+++ front/src/app/terms/page.tsx
@@ -1,15 +1,19 @@
 import { Header } from "@components/Header";
 import { FeedbackAlert } from "@components/FeedbackAlert";
 import { BottomNavi } from "@components/BottomNavi";
+import { Suspense } from"react";
 
 export default function MyPage(){
   return(
-    <div>
-      <Header />
-      <FeedbackAlert />
-      <h1>利用きやく</h1>
-      <h3>仮ページ...</h3>
-      <BottomNavi />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <Header />
+        <FeedbackAlert />
+        <h1>利用きやく</h1>
+        <h3>仮ページ...</h3>
+        <BottomNavi />
+      </div>
+    </Suspense>
+
   );
 }
