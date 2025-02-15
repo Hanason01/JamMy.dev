d6f9ffb07316c17f596536e1a4406c98adb21fb2
pucha <pucha@puchanoMac-mini.local>
2025-02-16 06:08:18 +0900

    レスポンスエラー解消

--- back/app/controllers/api/v1/users/projects_controller.rb
+++ back/app/controllers/api/v1/users/projects_controller.rb
@@ -6,6 +6,7 @@ class Api::V1::Users::ProjectsController < ApplicationController
       return render json: { error: "不正なパラメーターです" }, status: :bad_request
 
     end
+    Rails.logger.debug "渡されてきたフィルターやで！！！！！！！！！！: #{params[:filter].inspect}"
     projects =
       case params[:filter]
       when "my_projects"
@@ -34,7 +35,7 @@ class Api::V1::Users::ProjectsController < ApplicationController
       user_bookmarks = user_bookmarks_map
       project_likes = project_likes_map
       project_comments = project_comments_map
-      serialized = serialized_projects(projects, user_likes, project_likes, user_bookmarks, project_comments)
+      serialized = serialized_projects(projects, user_likes, user_bookmarks, project_likes, project_comments)
 
       render json: {
         data: serialized[:data],
